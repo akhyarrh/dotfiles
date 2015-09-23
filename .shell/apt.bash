1c40ce6883e53fd6a9ee4ec7c57c03f13af967d9
Akhyar Rahman Hidayat <akhyarrh@gmail.com>
2015-09-24 04:02:23 +0800

    Update apt related alias
    Add '-me' to multiple apt related command
    Easier to use it
    Ex:
    - update (sudo apt-get update)
    - update-me (sudo apt-get update; sudo apt-get upgrade -y; sudo apt-get dist-upgrade -y)

--- .shell/apt.bash
+++ .shell/apt.bash
@@ -1,14 +1,26 @@
 # apt related config
 
-# simple update
-alias update="sudo apt-get update; sudo apt-get upgrade -y; sudo apt-get dist-upgrade -y"
+# update
+alias update="sudo apt-get update"
 
-# just upgrade
-alias upgrade="sudo apt-get upgrade -y; sudo apt-get dist-upgrade -y"
+# install
+alias install="sudo apt-get install"
+
+# upgrade
+alias upgrade="sudo apt-get upgrade"
+
+# dist-upgrade
+alias dist-upgrade="sudo apt-get dist-upgrade"
+
+# update-me
+alias update-me="sudo apt-get update; sudo apt-get upgrade -y; sudo apt-get dist-upgrade -y"
+
+# upgrade-me
+alias upgrade-me="sudo apt-get upgrade -y; sudo apt-get dist-upgrade -y"
 
 # simple clean
 # Be careful, may break something
-alias clean="sudo apt-get autoremove; sudo apt-get autoclean"
+alias clean-me="sudo apt-get autoremove; sudo apt-get autoclean"
 
 # get list latest package installed
 alias apt-log="cat /var/log/dpkg.log | grep '\ install\ '"
