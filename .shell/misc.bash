ea28f7ec83b582cb8bacfe2f673ff0f4cb8ba757
Akhyar Rahman Hidayat <akhyarrh@gmail.com>
2015-09-25 21:48:26 +0800

    Update misc. bash alias
    Add miscelaneous bash alias
    Move 3rd party related alias to another file

--- .shell/misc.bash
+++ .shell/misc.bash
@@ -1,9 +1,50 @@
-# NVM
-source ~/.nvm/nvm.sh
+# c
+alias c="reset"
 
-# Chruby
-source /usr/local/share/chruby/chruby.sh
-source /usr/local/share/chruby/auto.sh
+# calc
+alias calc="bc -l"
 
-# hub by GitHub
-eval "$(hub alias -s)"
+# change-dns
+alias change-dns="sudo nano /etc/dhcp/dhclient.conf && sudo /etc/init.d/networking force-reload"
+
+# check-url
+alias check-url="sudo hostsblock-urlcheck"
+
+# df
+alias df="df -Tha --total"
+
+# edit
+alias edit="nano --tabstospaces --smooth --tabsize=2 --autoindent --undo --softwrap"
+
+# free
+alias free="free -mt"
+
+# header
+alias header='curl -I'
+
+# headerc
+alias headerc='curl -I --compress'
+
+# list
+alias list="ls -A1N"
+
+# mkdir
+alias mkdir="mkdir -pv"
+
+# my-ip
+alias myip="curl http://ipecho.net/plain; echo"
+
+# psf
+alias psf="ps aux | grep -v grep | grep -i -e VSZ -e"
+
+# reload
+alias reload="reset; source ~/.bashrc"
+
+# sublime
+alias sublime="/opt/sublime_text/sublime_text"
+
+# time
+alias time="date +"%d-%m-%Y_%T""
+
+# wget
+alias wget="wget -c --no-proxy"
