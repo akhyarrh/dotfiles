4d06c826feb5fd8eb09d02dec4f98db146fa7bf4
Akhyar Rahman Hidayat <akhyarrh@gmail.com>
2015-10-08 05:01:32 +0800

    Minor update navigation.bash
    Make shell linter quiet

--- .shell/navigation.bash
+++ .shell/navigation.bash
@@ -1,9 +1,9 @@
-PROJECT_FOLDER="~/project"
+PROJECT_FOLDER=~/project
 
 alias ..="cd .."
 
-alias project="cd $PROJECT_FOLDER"
+alias project="cd \$PROJECT_FOLDER"
 
-alias config="cd $PROJECT_FOLDER/dotfiles"
+alias config="cd \$PROJECT_FOLDER/dotfiles"
 
-alias blog="cd $PROJECT_FOLDER/akhyarrh.github.io"
+alias blog="cd \$PROJECT_FOLDER/akhyarrh.github.io"
