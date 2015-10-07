f8c471a4dec5c2a9a0356bd2c4910c175e0f278c
Akhyar Rahman Hidayat <akhyarrh@gmail.com>
2015-10-08 05:07:16 +0800

    Add youtube-dl.bash
    Contains related alias to youtube-dl

---
+++ .shell/youtube-dl.bash
@@ -0,0 +1,16 @@
+# youtube-dl
+
+#ytdl
+alias ytdl="youtube-dl"
+
+# ytdl-480
+alias ytdl-480="youtube-dl -f 'mp4[height<=?480]'"
+
+# ytdl-720
+alias ytdl-720="youtube-dl -f 'mp4[height<=?720]'"
+
+# ytdl-mp3
+# download video with best audio
+# extract audio
+# convert to mp3 with highest quality
+alias ytdl-mp3="youtube-dl -f 'bestaudio' -x --audio-format mp3 --audio-quality 320K"
