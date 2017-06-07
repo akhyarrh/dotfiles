093e36583caa6c27e82b1c156c5279c516493ee5
Akhyar Rahman Hidayat <akhyarrh@gmail.com>
2017-06-08 06:17:16 +0800

    Minor cleaning

--- .shell/navigation.bash
+++ .shell/navigation.bash
@@ -1,9 +1,1 @@
-PROJECT_FOLDER=~/project
-
 alias ..="cd .."
-
-alias project="cd \$PROJECT_FOLDER"
-
-alias config="cd \$PROJECT_FOLDER/dotfiles"
-
-alias blog="cd \$PROJECT_FOLDER/akhyarrh.github.io"
