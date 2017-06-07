dfcddd5b421a20c92d532cfd2fe400542d99827c
Akhyar Rahman Hidayat <akhyarrh@gmail.com>
2017-06-08 06:13:03 +0800

    Update apt command
    Use 'apt' instead of 'apt-get'
    Simpler, shorter, cooler

--- .shell/apt.bash
+++ .shell/apt.bash
@@ -1,26 +1,26 @@
 # apt related config
 
 # update
-alias update="sudo apt-get update"
+alias update="sudo apt update"
 
 # install
-alias install="sudo apt-get install"
+alias install="sudo apt install"
 
 # upgrade
-alias upgrade="sudo apt-get upgrade"
+alias upgrade="sudo apt upgrade"
 
 # dist-upgrade
-alias dist-upgrade="sudo apt-get dist-upgrade"
+alias dist-upgrade="sudo apt dist-upgrade"
 
 # update-me
-alias update-me="sudo apt-get update; sudo apt-get upgrade -y; sudo apt-get dist-upgrade -y"
+alias update-me="sudo apt update; sudo apt upgrade -y; sudo apt dist-upgrade -y"
 
 # upgrade-me
-alias upgrade-me="sudo apt-get upgrade -y; sudo apt-get dist-upgrade -y"
+alias upgrade-me="sudo apt upgrade -y; sudo apt dist-upgrade -y"
 
 # simple clean
 # Be careful, may break something
-alias clean-me="sudo apt-get autoremove; sudo apt-get autoclean"
+alias clean-me="sudo apt autoremove; sudo apt autoclean"
 
 # get list latest package installed
 alias apt-log="cat /var/log/dpkg.log | grep '\ install\ '"
